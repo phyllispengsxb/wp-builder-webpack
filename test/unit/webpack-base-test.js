0eacd6a5bf3c4499cd40e76923a79aad254ff7e7
phyllis <phyllispeng@163.com>
2021-04-09 19:17:20 +0800

    webpack builder

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,6 +5,6 @@ describe('webpack.base.js test case', ()=> {
 
     console.log(baseConfig);
     it('entry', () => {
-        // assert.equal(baseConfig.entry, '')
+        // assert.equal(baseConfig.entry,index, '')
     })
 })
